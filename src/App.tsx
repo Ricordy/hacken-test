37239ca9038f4731139c71cdcd9ec977b2023f2a
Barrocz <youberrody@gmail.com>
2024-07-12 16:22:50 +0100

    pagination and selectable currency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import { Table } from "antd";
+import {
+  GetProp,
+  Select,
+  Table,
+  TablePaginationConfig,
+  TableProps,
+} from "antd";
 import "./App.css";
+import { useEffect, useState } from "react";
+import { SorterResult } from "antd/es/table/interface";
+
+interface TableParams {
+  pagination?: TablePaginationConfig;
+  sortField?: SorterResult<number>["field"];
+  sortOrder?: SorterResult<number>["order"];
+  filters?: Parameters<GetProp<TableProps, "onChange">>[1];
+}
 
 function App() {
+  const [data, setData] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState("usd");
+  const [loading, setLoading] = useState(true);
+  const [tableParams, setTableParams] = useState<TableParams>({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+  });
+
   const columns = [
     { title: "Name", dataIndex: "name", key: "name" },
     {
@@ -16,20 +41,54 @@ function App() {
     },
   ];
 
-  const data = [
-    { name: "Name", current_price: "1", circulating_supply: "2" },
-    { name: "bit", current_price: "2", circulating_supply: "1" },
-  ];
+  const handleTableChange: TableProps["onChange"] = (
+    pagination,
+    filters,
+    sorter
+  ) => {
+    setTableParams({
+      pagination,
+      filters,
+      sortOrder: Array.isArray(sorter) ? undefined : sorter.order,
+      sortField: Array.isArray(sorter) ? undefined : sorter.field,
+    });
+  };
+
+  const handleCurrencyChange = (value: string) => {
+    setSelectedCurrency(value);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selectedCurrency}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [selectedCurrency]);
 
   return (
     <>
       <section>
+        <Select
+          defaultValue="usd"
+          style={{ width: 120 }}
+          onChange={handleCurrencyChange}
+          options={[
+            { value: "usd", label: "USD" },
+            { value: "eur", label: "EUR" },
+          ]}
+        />
         <Table
           dataSource={data}
           columns={columns}
+          loading={loading}
           rowKey="id"
-          pagination={false}
+          pagination={tableParams.pagination}
           style={{ width: "100vw" }}
+          onChange={handleTableChange}
         />
       </section>
     </>
