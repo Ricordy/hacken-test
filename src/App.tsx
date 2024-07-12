0410bc1e6088aec5a9b061b67778be7c252dabb1
Barrocz <youberrody@gmail.com>
2024-07-12 17:51:56 +0100

    codemirror working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import {
+  Avatar,
   GetProp,
   Select,
   Space,
   Table,
   TablePaginationConfig,
   TableProps,
+  Typography,
 } from "antd";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { SorterResult } from "antd/es/table/interface";
+import { javascript } from "@codemirror/lang-javascript";
+import Controlled from "@uiw/react-codemirror";
 
+const { Title } = Typography;
+
+// Type for the parameters that define the table
 interface TableParams {
   pagination?: TablePaginationConfig;
   sortField?: SorterResult<number>["field"];
@@ -18,6 +25,7 @@ interface TableParams {
 }
 
 function App() {
+  // STATE VARIABLES
   const [data, setData] = useState([]);
   const [selectedCurrency, setSelectedCurrency] = useState("usd");
   const [sorting, setSorting] = useState("desc");
@@ -26,15 +34,31 @@ function App() {
     pagination: {
       current: 1,
       pageSize: 10,
+      total: 10000,
     },
   });
 
+  // Variables for the columns that defines rendenring logic and parameters to show
   const columns = [
-    { title: "Name", dataIndex: "name", key: "name" },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (item: unknown, record: any) => {
+        return (
+          <Space size={5}>
+            <Avatar src={record.image} /> {" " + item}
+          </Space>
+        );
+      },
+    },
     {
       title: "Current Price",
       dataIndex: "current_price",
       key: "current_price",
+      render: (item: any, record: any) => {
+        return <div>{item + " " + selectedCurrency} </div>;
+      },
     },
     {
       title: "Circulating supply",
@@ -43,6 +67,7 @@ function App() {
     },
   ];
 
+  // Call back function for the Table component
   const handleTableChange: TableProps["onChange"] = (
     pagination,
     filters,
@@ -66,19 +91,20 @@ function App() {
 
   useEffect(() => {
     fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selectedCurrency}&order=market_cap_${sorting}`
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selectedCurrency}&order=market_cap_${sorting}&per_page=${tableParams.pagination?.pageSize}&page=${tableParams.pagination?.current}&sparkline=false`
     )
       .then((response) => response.json())
       .then((data) => {
         setData(data);
         setLoading(false);
       });
-  }, [selectedCurrency, sorting]);
+  }, [selectedCurrency, sorting, tableParams.pagination]);
 
   return (
     <main>
       <section>
         <Space direction="vertical" size={20} style={{ width: "100%" }}>
+          <Title level={2}>Coins & Markets</Title>
           <Space size={15}>
             <Select
               defaultValue="usd"
@@ -111,8 +137,167 @@ function App() {
           />
         </Space>
       </section>
+      <section>
+        <Space direction="vertical" size={20} style={{ width: "100%" }}>
+          <Title level={2}>Coins & Markets</Title>
+          <Controlled value={code} extensions={[javascript({ jsx: true })]} />
+        </Space>
+      </section>
     </main>
   );
 }
 
 export default App;
+
+const code = `
+import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Select,
+  Space,
+  Table,
+  TablePaginationConfig,
+  TableProps,
+  Typography,
+} from "antd";
+import { SorterResult } from "antd/es/table/interface";
+import { Controlled as CodeMirror } from "@uiw/react-codemirror";
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/javascript/javascript';
+import "./App.css";
+
+const { Title } = Typography;
+
+interface TableParams {
+  pagination?: TablePaginationConfig;
+  sortField?: SorterResult<number>["field"];
+  sortOrder?: SorterResult<number>["order"];
+  filters?: Parameters<GetProp<TableProps, "onChange">>[1];
+}
+
+function App() {
+  const [data, setData] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState("usd");
+  const [sorting, setSorting] = useState("desc");
+  const [loading, setLoading] = useState(true);
+  const [tableParams, setTableParams] = useState<TableParams>({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+      total: 10000,
+    },
+  });
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (item: any, record: any) => {
+        return (
+          <Space size={5}>
+            <Avatar src={record.image} /> {" " + item}
+          </Space>
+        );
+      },
+    },
+    {
+      title: "Current Price",
+      dataIndex: "current_price",
+      key: "current_price",
+      render: (item: any, record: any) => {
+        return <div>{item + " " + selectedCurrency} </div>;
+      },
+    },
+    {
+      title: "Circulating supply",
+      dataIndex: "circulating_supply",
+      key: "circulating_supply",
+    },
+  ];
+
+  const handleTableChange: TableProps["onChange"] = (
+    pagination,
+    filters,
+    sorter
+  ) => {
+    setTableParams({
+      pagination,
+      filters,
+      sortOrder: Array.isArray(sorter) ? undefined : sorter.order,
+      sortField: Array.isArray(sorter) ? undefined : sorter.field,
+    });
+  };
+
+  const handleCurrencyChange = (value: string) => {
+    setSelectedCurrency(value);
+  };
+
+  const handleSortingChange = (value: string) => {
+    setSorting(value);
+  };
+
+  useEffect(() => {
+    console.log("current page: ", tableParams.pagination?.current);
+
+    fetch(
+      \`https://api.coingecko.com/api/v3/coins/markets?vs_currency=\${selectedCurrency}&order=market_cap_\${sorting}&per_page=\${tableParams.pagination?.pageSize}&page=\${tableParams.pagination?.current}&sparkline=false\`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [selectedCurrency, sorting, tableParams.pagination]);
+
+  return (
+    <main>
+      <section>
+        <Space direction="vertical" size={20} style={{ width: "100%" }}>
+          <Title level={2}>Coins & Markets</Title>
+          <Space size={15}>
+            <Select
+              defaultValue="usd"
+              style={{ width: 180 }}
+              onChange={handleCurrencyChange}
+              options={[
+                { value: "usd", label: "USD" },
+                { value: "eur", label: "EUR" },
+              ]}
+            />
+            <Select
+              defaultValue="desc"
+              style={{ width: 180 }}
+              onChange={handleSortingChange}
+              options={[
+                { value: "asc", label: "Market cap ascending" },
+                { value: "desc", label: "Market cap descending" },
+              ]}
+            />
+          </Space>
+
+          <Table
+            dataSource={data}
+            columns={columns}
+            loading={loading}
+            rowKey="id"
+            pagination={tableParams.pagination}
+            style={{ width: "100%" }}
+            onChange={handleTableChange}
+          />
+        </Space>
+      </section>
+      <CodeMirror
+        value={code}
+        options={{
+          mode: 'typescript',
+          theme: 'material',
+          lineNumbers: true,
+        }}
+      />
+    </main>
+  );
+}
+
+export default App;`;
