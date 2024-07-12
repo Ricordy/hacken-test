25c320713a2d9ac5bbf1a5988691a1322fb28c77
Barrocz <youberrody@gmail.com>
2024-07-12 20:29:00 +0100

    fixed wrong code presentation

--- src/App.tsx
+++ src/App.tsx
@@ -34,10 +34,10 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [marketCapDiff, setMarketCapDiff] = useState(0.0);
   const [coinsToCompare, setCoinsToCompare] = useState({
-    coin1: data[0]?.nome,
-    coin2: data[1]?.nome,
-    coin1MC: data[0]?.market_cap,
-    coin2MC: data[1]?.market_cap,
+    coin1: "",
+    coin2: "",
+    coin1MC: 0,
+    coin2MC: 0,
   });
   const [tableParams, setTableParams] = useState<TableParams>({
     pagination: {
@@ -285,10 +285,6 @@ import {
   Typography,
 } from "antd";
 import { SorterResult } from "antd/es/table/interface";
-import { Controlled as CodeMirror } from "@uiw/react-codemirror";
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/mode/javascript/javascript';
 import "./App.css";
 
 const { Title } = Typography;
@@ -307,6 +303,13 @@ function App() {
   const [selectedCurrency, setSelectedCurrency] = useState("usd");
   const [sorting, setSorting] = useState("desc");
   const [loading, setLoading] = useState(true);
+  const [marketCapDiff, setMarketCapDiff] = useState(0.0);
+  const [coinsToCompare, setCoinsToCompare] = useState({
+    coin1: "",
+    coin2:"",
+    coin1MC: 0,
+    coin2MC: 0,
+  });
   const [tableParams, setTableParams] = useState<TableParams>({
     pagination: {
       current: 1,
@@ -321,7 +324,7 @@ function App() {
       title: "Name",
       dataIndex: "name",
       key: "name",
-      render: (item: any, record: any) => {
+      render: (item: unknown, record: any) => {
         return (
           <Space size={5}>
             <Avatar src={record.image} /> {" " + item}
@@ -333,7 +336,7 @@ function App() {
       title: "Current Price",
       dataIndex: "current_price",
       key: "current_price",
-      render: (item: any, record: any) => {
+      render: (item: any) => {
         return <div>{item + " " + selectedCurrency} </div>;
       },
     },
@@ -358,7 +361,7 @@ function App() {
     });
   };
 
-  const handleCurrencyChange = (value: string) => {
+ const handleCurrencyChange = (value: string) => {
     setSelectedCurrency(value);
   };
 
@@ -366,10 +369,36 @@ function App() {
     setSorting(value);
   };
 
+  const onChange1 = (value: string) => {
+    const item = data.filter((i) => i.name === value);
+    console.log("item 0 : ", item[0]);
+
+    if (item.length > 0) {
+      setCoinsToCompare((prev) => ({
+        ...prev,
+        coin1MC: Number(item[0].market_cap),
+        coin1: item[0].name,
+      }));
+    }
+  };
+
+  const onChange2 = (value: string) => {
+    const item = data.filter((i) => i.name === value);
+    if (item.length > 0) {
+      setCoinsToCompare((prev) => ({
+        ...prev,
+        coin2MC: Number(item[0].market_cap),
+        coin2: item[0].name,
+      }));
+    }
+  };
+
+  const onSearch = (value: string) => {
+    console.log("search:", value);
+  };
+
   // UseEffect to fetch the API when the state is changed or after first loading the page
   useEffect(() => {
-    console.log("current page: ", tableParams.pagination?.current);
-
     fetch(
       \`https://api.coingecko.com/api/v3/coins/markets?vs_currency=\${selectedCurrency}&order=market_cap_\${sorting}&per_page=\${tableParams.pagination?.pageSize}&page=\${tableParams.pagination?.current}&sparkline=false\`
     )
@@ -377,12 +406,98 @@ function App() {
       .then((data) => {
         setData(data);
         setLoading(false);
+        setCoinsToCompare({
+          coin1MC: Number(data[0]?.market_cap),
+          coin2MC: Number(data[1]?.market_cap),
+          coin1: data[0]?.name,
+          coin2: data[1]?.name,
+        });
       });
   }, [selectedCurrency, sorting, tableParams.pagination]);
+
+  useEffect(() => {
+    if (coinsToCompare.coin1MC > coinsToCompare.coin2MC) {
+      setMarketCapDiff(coinsToCompare.coin1MC / coinsToCompare.coin2MC);
+    } else if (coinsToCompare.coin1MC < coinsToCompare.coin2MC) {
+      setMarketCapDiff(
+        -Math.abs(coinsToCompare.coin2MC / coinsToCompare.coin1MC)
+      );
+    } else {
+      setMarketCapDiff(0);
+    }
+
+    return () => {};
+  }, [coinsToCompare.coin1MC, coinsToCompare.coin2MC]);
 
   // Returning HTML
   return (
     <main>
+      <section>
+        <Space direction="vertical" size={20} style={{ width: "100%" }}>
+          <Title level={2}>Market cap comparasion</Title>
+          <Card style={{ width: "100%" }}>
+            <Space>
+              <Select
+                placeholder="Choose the currency"
+                onChange={onChange1}
+                onSearch={onSearch}
+                style={{ width: "200px" }}
+              >
+                {data.map((cryptocurrency) => {
+                  return (
+                    <Select.Option
+                      value={cryptocurrency.name}
+                      key={cryptocurrency.id}
+                    >
+                      {cryptocurrency.name.toString()}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+              <Select
+                showSearch
+                placeholder="Choose the currency"
+                onChange={onChange2}
+                onSearch={onSearch}
+                style={{ width: "200px" }}
+              >
+                {data.map((cryptocurrency) => {
+                  return (
+                    <Select.Option
+                      value={cryptocurrency.name}
+                      key={cryptocurrency.id}
+                    >
+                      {cryptocurrency.name.toString()}
+                    </Select.Option>
+                  );
+                })}
+              </Select>
+            </Space>
+
+            <Title level={3}>
+              How many times{" "}
+              <text className={marketCapDiff > 0 ? "text-green" : "text-red"}>
+                {coinsToCompare.coin1}
+              </text>{" "}
+              market cap is {marketCapDiff > 0 ? "bigger" : "smaller"} than{" "}
+              <text className={marketCapDiff < 0 ? "text-green" : "text-red"}>
+                {coinsToCompare.coin2}
+              </text>
+              ?
+            </Title>
+            <Title>
+              <NumericFormat
+                value={marketCapDiff}
+                decimalScale={2}
+                decimalSeparator="."
+                prefix={"x"}
+                displayType="text"
+                allowNegative
+              />
+            </Title>
+          </Card>
+        </Space>
+      </section>
       <section>
         <Space direction="vertical" size={20} style={{ width: "100%" }}>
           <Title level={2}>Coins & Markets</Title>
@@ -420,7 +535,7 @@ function App() {
       </section>
       <section>
         <Space direction="vertical" size={20} style={{ width: "100%" }}>
-          <Title level={2}>Coins & Markets</Title>
+          <Title level={2}>App source code</Title>
           <Controlled value={code} extensions={[javascript({ jsx: true })]} />
         </Space>
       </section>
